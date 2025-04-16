4d449510857ba854c3b68e90ba9e102b5632ba4d
ayushsahu007 <ayushsahu007ay@gmail.com>
2025-04-16 18:08:32 +0530

    1st commit

---
+++ src/test/java/com/example/git_project_demo/GitProjectDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git_project_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitProjectDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
